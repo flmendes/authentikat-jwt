1817d4af61fbac449eac203be4b3cc77c54cfd5d
Flavio Mendes <flmendes@gmail.com>
2015-02-10 01:56:54 -0200

    Implemented Algorithm for RS256

--- src/main/scala/authentikat/jwt/JsonWebSignature.scala
+++ src/main/scala/authentikat/jwt/JsonWebSignature.scala
@@ -4,7 +4,7 @@ import java.security.spec.PKCS8EncodedKeySpec
 import java.security.{KeyFactory, PrivateKey, Signature}
 import javax.crypto.spec.SecretKeySpec
 import javax.crypto.Mac
-import org.apache.commons.codec.binary.{Base64, Hex}
+import org.apache.commons.codec.binary.{StringUtils, Base64, Hex}
 
 /**
  * Json Web Algorithms for Encrypting JWS.
@@ -26,18 +26,18 @@ object JsonWebSignature {
       case "HS256" => apply(HS256, data, key)
       case "HS384" => apply(HS384, data, key)
       case "HS512" => apply(HS512, data, key)
-      case "RSAWITHSHA1" => apply(RSAWITHSHA1, data, key)
-      case "none" => apply(none, data, key)
+      case "RS256" => apply(RS256, data, key)
+      case none  => apply(none, data, key)
       case x => throw new UnsupportedOperationException(x + " is an unknown or unimplemented JWT algo key")
     }
   }
 
   def apply(algorithm: Algorithm, data: String, key: String = null): Array[Byte] = {
     algorithm match {
-      case HS256 => HmacSha("HmacSHA256", data, key)
+      case HS256 => HmacSha("HS256", data, key)
       case HS384 => HmacSha("HmacSHA384", data, key)
       case HS512 => HmacSha("HmacSHA512", data, key)
-      case RSAWITHSHA1 => RsaSha1("SHA1withRSA", data, key)
+      case RS256 => RsaSha1("RS256", data, key)
       case none => Array.empty[Byte]
       case x => throw new UnsupportedOperationException(x + " is an unknown or unimplemented JWT algo key")
     }
@@ -45,19 +45,17 @@ object JsonWebSignature {
 
   private case object RsaSha1 {
     def apply(algorithm: String, data: String, key: String): Array[Byte] = {
-      val signer = Signature.getInstance("SHA1withRSA");
+      val signer = Signature.getInstance("SHA256withRSA");
       signer.initSign(getPemPrivateKey(key))
       signer.update(data.getBytes)
       signer.sign()
     }
 
-    def getPemPrivateKey(key: String, algorithm: String = "RSA"): PrivateKey = {
+    def getPemPrivateKey(key: String): PrivateKey = {
       val _key = key.replace("-----BEGIN PRIVATE KEY-----\n", "")
-      val privKeyPEM = _key.replace("-----END PRIVATE KEY-----", "")
-      val b64: Base64 = new Base64()
-      val decoded = b64.decode(privKeyPEM)
-      val spec: PKCS8EncodedKeySpec = new PKCS8EncodedKeySpec(decoded)
-      val kf = KeyFactory.getInstance(algorithm);
+      val strPrivatePemKey = _key.replace("-----END PRIVATE KEY-----\n", "")
+      val spec = new PKCS8EncodedKeySpec(Base64.decodeBase64(strPrivatePemKey))
+      val kf = KeyFactory.getInstance("RSA");
       kf.generatePrivate(spec)
     }
   }
@@ -83,7 +81,7 @@ object JsonWebSignature {
 
   case object HS512 extends Algorithm
 
-  case object RSAWITHSHA1 extends Algorithm;
+  case object RS256 extends Algorithm;
 
   //  private sealed abstract class UnimplementedAlgorithm extends Algorithm
   //  private case object RS256 extends UnimplementedAlgorithm //Recommended implementation
